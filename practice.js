802d162a5e564c59749ab6e1942307d7d5059911
Tate Stone <tatejamesstone@gmail.com>
2019-08-13 14:11:02 -0600

    Answered first 9 questions

--- practice.js
+++ practice.js
@@ -2,31 +2,31 @@
 
 //Create a variable called myName that is a string data type
 
-//Code here
+let myName = "Tate"
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-//Code here
+let myAge = 23
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-//Code here
+let lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-//Code here
+let greatestFear = undefined
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
-//Code here
+let devMountainGoal = null
 
 //////////////////PROBLEM 6////////////////////
 
@@ -36,20 +36,27 @@
 //plus the value of the name parameter.
 
 //Code here
+function greeting (name) {
 
+  return("Hello, " + name)
+
+}
+greeting("Trogdor")
 //////////////////PROBLEM 7////////////////////
 
 //Rewrite the function greeting as a function expression.
 //Name it newGreeting.
 
-//Code Here
-
+var newGreeting = function(name){
+  return("Hello, " + name)
+}
+newGreeting("Trogdor")
 //////////////////PROBLEM 8////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-//Code Here
+let groceries = ['apples', 'milk', 'eggs', 'bread']
 
 //////////////////PROBLEM 9////////////////////
 
@@ -58,7 +65,10 @@
 //name (a string), color (a string), age (a number),
 //and goodBoy (a boolean).
 
-//Code Here
+let dog = {name: "Rosie",
+color: "White and Brown",
+age: 42
+goodBoy: false}
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
