43265057f6947c4be8b4845fd512b28dbabb4540
Tate Stone <tatejamesstone@gmail.com>
2019-08-13 15:05:50 -0600

    Finished

--- practice.js
+++ practice.js
@@ -113,7 +113,7 @@ function add(num1, num2){
 //storing the result in the variable mathSum.
 
 
-var mathSum = (3 + 4)
+let mathSum = (3 + 4)
 
 //////////////////PROBLEM 12////////////////////
 
@@ -155,28 +155,27 @@ function pond() {
 //as strings.
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let globalScope = ["duck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathroomScope = ["duck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathtubScope = ["duck", "sailorDuck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let pondScope = ["duck", "realDuck"];
 
 //////////////////PROBLEM 14////////////////////
 
 //Create a variable called age with your age assigned to you
 
-// Code Here
+let age = 23
 
 // FLASH FORWARD TO NEXT YEAR
 // reassign the value of age to be one greater than it was, because, we all get older
 
-// Code Here
+age = 24
 
 // Good news! We can live forever. Set your age to 999
-
-// Code Here 
+age = 999
